caf142732e31e2da02a63d9f61239d8a9272b9bc
“CharlotteMD” <charlottemdavies@hotmail.co.uk>
2018-01-15 14:40:03 +0000

    added registration form

--- models/user.js
+++ models/user.js
@@ -2,14 +2,12 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-  firstName: { type: String, minlength: 2, required: true, trim: true },
-  lastName: { type: String, minlength: 2, required: true, trim: true },
-  email: { type: String, required: true, trim: true, unique: true },
-  username: { type: String, required: true, trim: true, unique: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   image: { type: String, required: true },
   seatPlan: { type: String, required: true },
-  diet: String,
   rsvps: String
 }, {
   timestamps: true
