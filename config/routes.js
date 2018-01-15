cc7246d3c2c54531dd27f14a512188e954988376
“CharlotteMD” <charlottemdavies@hotmail.co.uk>
2018-01-15 11:48:10 +0000

    routes updated

--- config/routes.js
+++ config/routes.js
@@ -7,74 +7,66 @@ const questions = require('../controllers/questions');
 const secureRoute = require('../lib/secureRoute');
 
 
-// User Pages
-
 // Homepage
 router.get('/', (req, res) => res.render('statics/index'));
 
-// router.route('/users')
-// //  homepage - why do I need duplicate?
-//   .get(users.index)
-//   .post(secureRoute, users.create);
-//   // dont want all users displayed!
-//   // also want login page success to redirect here
-
+// Register as a user
 router.route('/users/register')
-// user register page
   .get(secureRoute, users.new);
   .get(registrations.new);
   .post(registrations.create);
   // should redirect to login page
 
+// Login
 router.route('/users/login')
-// user login page
   .get(sessions.new);
   .post(sessions.create);
 
+// Logout
 router.route('/logout')
-// user logout page
   .get(sessions.delete);
 
+// Show user Page
 router.route('/users/:id')
-// user profile page
-  .get(users.id.show);
-  // only need the one user to show so do I need user.id?
-  .delete(secureRoute, users.id.delete);
-  // also want to be able to update wedding info with linked user id from this page
-  .get(weddings.id.show);
-  // only want some weddings linked to this id
-  .delete(secureRoute, weddings.id.delete);
+  .get(users.show)
+  .put(secureRoute, users.update)
+  .delete(secureRoute, users.delete);
 
+// Show Wedding page including all questions and comments
+router.route('/weddings/:id')
+  .get(weddings.show)
+  .put(secureRoute, weddings.update)
+  .delete(secureRoute, weddings.delete);
 
+// Edit User
+// Only accessible by user
 router.route('/users/:id/edit')
   .get(secureRoute, users.edit);
-  .put(secureRoute, users.id.update);
-  // redirects back to user/:id
-  // only want to update the one user so do I need user.id?
 
+// Edit Wedding
+// Only accessible by createdby or fiance
 router.route('/weddings/:id/edit')
   .get(secureRoute, weddings.edit);
-  .put(secureRoute, weddings.update);
-  .get(secureRoute, questions.edit);
-  .put(secureRoute, questions.update);
-  // redirects back to user/:id
 
-// Questions Pages
+// Edit Answers
+// Only accessible by createdby or wedding createdby or fiance
+router.route('/answers/:id/edit')
+  .get(secureRoute, answers.edit);
 
-router.route('/weddings/:id/')
-// Questions are linked to a specific wedding
-  .post(questions.createAnswer);
-  .get(questions.show);
-  .get(answers.show);
+// New Wedding
+// Show on user page
+router.route('/weddings/new')
+  .get(secureRoute, weddings.new);
 
-router.route('/users/:id/weddings/:id/questions/new')
+// New question
+// Show on Wedding page
+router.route('/questions/new')
   .get(secureRoute, questions.new);
-  .post(questions.create);
 
-router.route('/users/:id/weddings/:id/questions/new')
-
-router.route('/users/:id/weddings/:id/')
-  .delete(answers.deleteAnswer);
+// New answer
+// Show on wedding page 
+router.route('/answers/new')
+  .get(secureRoute, answers.new);
 
 router.all('*', (req, res) => res.notFound());
 
